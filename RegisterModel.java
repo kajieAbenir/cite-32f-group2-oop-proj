e89b5dafbea248c8095909d9bc021e2a7ce054b2
clesterbadz <146814262+clesterbadz@users.noreply.github.com>
2023-12-09 23:16:13 +0800

    Update RegisterModel.java

--- RegisterModel.java
+++ RegisterModel.java
@@ -2,6 +2,50 @@ package oop.project;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class RegisterModel {
+
+    private static final String FILE_PATH = "users.dat";
+
+    public boolean registerUser(String fullName, String emailAddress, String password) {
+        if (isValidInput(fullName, emailAddress, password)) {
+            appendUserDataToTextArea("Registered User:");
+            appendUserDataToTextArea("Full Name: " + fullName);
+            appendUserDataToTextArea("Email Address: " + emailAddress);
+
+            // Note: In a real-world scenario, you should not store passwords in plain text.
+            // Consider using a secure hashing algorithm and salt to store passwords securely.
+
+            // Save the user data to a file
+            saveUserDataToFile(fullName, emailAddress, password);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isValidInput(String fullName, String emailAddress, String password) {
+        return !fullName.isEmpty() && !emailAddress.isEmpty() && !password.isEmpty();
+    }
+
+    private void saveUserDataToFile(String fullName, String emailAddress, String password) {
+        try {
+            String userData = "Full Name: " + fullName + "\n" +
+                    "Email Address: " + emailAddress + "\n" +
+                    "Password: " + password + "\n\n";
+
+            Files.write(Paths.get(FILE_PATH), userData.getBytes(), java.nio.file.StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }package oop.project;
+
+import javax.swing.*;
+import java.io.IOException;
 import java.nio.file.*;
 
 class RegisterModel {

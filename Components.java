0b7960f4e3a498ae5402c574cfbdc13cd4b7e2d4
clesterbadz <146814262+clesterbadz@users.noreply.github.com>
2023-12-09 23:21:52 +0800

    Update Components.java

--- Components.java
+++ Components.java
@@ -75,3 +75,59 @@ public class Components {
         mainFrame.repaint();
     }
 }
+
+
+
+
+// package oop.project;
+
+// import javax.swing.*;
+// import java.awt.*;
+
+// class Components {
+//     public static JFrame mainFrame = new JFrame();
+
+//     Components() {
+//         addFrame();
+//         mainFrame.setVisible(true);
+//     }
+
+//     static void addFrame() {
+//         mainFrame.setSize(1200, 800);
+//         mainFrame.setLocationRelativeTo(null);
+//         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         mainFrame.setLayout(null);
+//         mainFrame.setResizable(false);
+//     }
+
+//     static void addPanel(JPanel panel, int xAxis, int yAxis, int width, int height, String hexColor) {
+//         panel.setBackground(Color.decode(hexColor));
+//         panel.setBounds(xAxis, yAxis, width, height);
+//     }
+
+//     static void addLabel(JPanel panel, JLabel label, int xAxis, int yAxis, int width, int height) {
+//         label.setBounds(xAxis, yAxis, width, height);
+//         panel.add(label);
+//     }
+
+//     static void addTextField(JPanel panel, JTextField textField, int xAxis, int yAxis, int width, int height) {
+//         textField.setBounds(xAxis, yAxis, width, height);
+//         panel.add(textField);
+//     }
+
+//     static void addButton(JPanel panel, JButton button, int xAxis, int yAxis, int width, int height) {
+//         button.setBounds(xAxis, yAxis, width, height);
+//         panel.add(button);
+//     }
+
+//     static void addTextArea(JPanel panel, JTextArea textArea, int xAxis, int yAxis, int width, int height) {
+//         textArea.setBounds(xAxis, yAxis, width, height);
+//         panel.add(textArea);
+//     }
+
+//     static void clearFrame() {
+//         mainFrame.getContentPane().removeAll();
+//         mainFrame.validate();
+//         mainFrame.repaint();
+//     }
+// }
